8022a151331591ce60d85d9931342852959d9d18
apple <apple@MacBook-Pro.local>
2020-09-20 19:43:44 +0800

    all updated

--- upload.sh
+++ upload.sh
@@ -3,9 +3,6 @@ git lfs track data
 git lfs track CRF
 git add .gitattributes
 git add .
-#git add crf_lstm.ipynb
-#git add loaddata.ipynb
-#git add upload.sh
 git commit -m 'all updated'
 git remote rm origin
 git remote add origin https://github.com/VentureRice/TianchiNER.git
