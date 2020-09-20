ece5f0a98302ce920adb2f695500ffc32218b17c
apple <apple@MacBook-Pro.local>
2020-09-20 17:21:28 +0800

    all updated

---
+++ NER_Data_0917.md
@@ -0,0 +1,14 @@
+FileName: round1_test.zip
+
+Size:320.38KB
+
+Link:https://tianchi-competition.oss-cn-hangzhou.aliyuncs.com/531824/round1_test.zip
+
+
+
+FileName:round1_train.zip
+
+Size:1.11MB
+
+Link:https://tianchi-competition.oss-cn-hangzhou.aliyuncs.com/531824/round1_train.zip
+
--- upload.sh
+++ upload.sh
@@ -1,8 +1,12 @@
 git init
-git add crf_lstm.ipynb
-git add loaddata.ipynb
-git add upload.sh
-git commit -m 'first updated'
+git lfs track data
+git lfs track CRF
+git add .gitattributes
+git add .
+#git add crf_lstm.ipynb
+#git add loaddata.ipynb
+#git add upload.sh
+git commit -m 'all updated'
 git remote rm origin
 git remote add origin https://github.com/VentureRice/TianchiNER.git
 git push origin master
