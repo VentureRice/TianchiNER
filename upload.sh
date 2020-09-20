e49aed643e5b05f8088d88af52b85de60ec297a5
apple <apple@MacBook-Pro.local>
2020-09-20 17:17:34 +0800

    first updated

---
+++ upload.sh
@@ -0,0 +1,8 @@
+git init
+git add crf_lstm.ipynb
+git add loaddata.ipynb
+git add upload.sh
+git commit -m 'first updated'
+git remote rm origin
+git remote add origin https://github.com/VentureRice/TianchiNER.git
+git push origin master
